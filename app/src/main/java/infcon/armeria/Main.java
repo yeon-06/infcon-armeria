87e3ed94f11ecc3579c10da676609a9197ce1014
yeon-06 <rnjstldus2@gmail.com>
2022-08-26 16:04:35 +0900

    feat: /infcon 접속 시 Hello, Armeria 응답

--- app/src/main/java/infcon/armeria/Main.java
+++ app/src/main/java/infcon/armeria/Main.java
@@ -1,8 +1,22 @@
 package infcon.armeria;
 
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+
+import java.util.concurrent.CompletableFuture;
+
 public final class Main {
 
     public static void main(String[] args) {
+        // 생성자보다 builder 같은 static method 선호
+        Server server = Server.builder()
+                .http(8080)
+                .service("/infcon", (context, request) -> {
+                    return HttpResponse.of("Hello, Armeria!");
+                })
+                .build();
 
+        CompletableFuture<Void> future = server.start();
+        future.join();
     }
 }
