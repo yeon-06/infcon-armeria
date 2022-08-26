20c2c3314731f2c8340adc1dbaf8e4d8888c4e12
yeon-06 <rnjstldus2@gmail.com>
2022-08-26 16:35:19 +0900

    feat: 비동기 통신과 콜백 사용해보기

--- app/src/main/java/infcon/armeria/Backend.java
+++ app/src/main/java/infcon/armeria/Backend.java
@@ -1,5 +1,29 @@
 package infcon.armeria;
 
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+
+import java.time.Duration;
+
 public final class Backend {
 
+    private final Server server;
+
+    public static Backend of(String name, int port) {
+        return new Backend(name, port);
+    }
+
+    private Backend(String name, int port) {
+        server = Server.builder()
+                .http(port)
+                .service("/foo", (ctx, req) -> {
+                    HttpResponse response = HttpResponse.of("response from: " + name);
+                    return HttpResponse.delayed(response, Duration.ofSeconds(3));
+                })
+                .build();
+    }
+
+    public void start() {
+        server.start().join();
+    }
 }
--- app/src/test/java/infcon/armeria/BackendTest.java
+++ app/src/test/java/infcon/armeria/BackendTest.java
@@ -1,11 +1,40 @@
 package infcon.armeria;
 
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.HttpResponse;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
 
 class BackendTest {
 
     @Test
-    void backend() {
+    void backend() throws InterruptedException {
+        Backend foo = Backend.of("foo", 9000);
+        foo.start();
 
+        WebClient webClient = WebClient.of("http://127.0.0.1:9000");
+
+        /* 비동기 통신임을 기억하자.
+         * /foo에서는 3초 뒤에 response 값을 받는다.
+         * 그 전까지는 httpResponse 껍데기만 존재한다.
+         */
+        HttpResponse httpResponse = webClient.get("/foo"); // post, get, put, delete, ... 의 get
+
+        /*
+         * httpResponse를 이용해 또 다른 껍데기를 만들었다.
+         * 이를 이용해 콜백을 해보겠다.
+         */
+        CompletableFuture<AggregatedHttpResponse> future = httpResponse.aggregate();
+        future.thenAccept(aggregatedHttpResponse -> {
+            System.out.println(Thread.currentThread().getName());
+            sendBackToTheOriginalClient(aggregatedHttpResponse);
+        });
+
+        Thread.sleep(Long.MAX_VALUE);
+    }
+
+    private void sendBackToTheOriginalClient(AggregatedHttpResponse aggregatedHttpResponse) {
     }
 }
